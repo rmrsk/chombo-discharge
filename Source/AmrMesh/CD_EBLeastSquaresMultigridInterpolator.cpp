dcaee2887053450c393fc73b147213d2683e7dad
Robert Marskar <robert.marskar@sintef.no>
2023-05-22 21:05:29 +0200

    Remove barrier

--- Source/AmrMesh/CD_EBLeastSquaresMultigridInterpolator.cpp
+++ Source/AmrMesh/CD_EBLeastSquaresMultigridInterpolator.cpp
@@ -96,7 +96,6 @@ EBLeastSquaresMultigridInterpolator::coarseFineInterp(LevelData<EBCellFAB>&     
                                                       const Interval              a_variables) const noexcept
 {
   CH_TIMERS("EBLeastSquaresMultigridInterpolator::coarseFineInterp");
-  CH_TIMER("EBLeastSquaresMultigridInterpolator::barrier", t0);  
   CH_TIMER("EBLeastSquaresMultigridInterpolator::copy_exchange", t1);
   CH_TIMER("EBLeastSquaresMultigridInterpolator::regular_interp", t2);
   CH_TIMER("EBLeastSquaresMultigridInterpolator::irregular_interp", t3);
@@ -108,10 +107,6 @@ EBLeastSquaresMultigridInterpolator::coarseFineInterp(LevelData<EBCellFAB>&     
   if (a_phiFine.ghostVect() != m_ghostVector) {
     MayDay::Error("EBLeastSquaresMultigridInterpolator::coarseFineInterp -- number of ghost cells do not match!");
   }
-
-  CH_START(t0);
-  ParallelOps::barrier();
-  CH_STOP(t0);  
 
   // Interpolate all variables near the EB. We will copy a_phiCoar to m_coarData which holds the data on the coarse grid cells around each fine-grid
   // patch. Note that m_coarData provides a LOCAL view of the coarse grid around each fine-level patch, so we can apply the stencils directly.
