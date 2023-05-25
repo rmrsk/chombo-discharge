d27232c76adb3cac738087b20318f8d1d646620d
Robert Marskar <robert.marskar@gmail.com>
2023-05-25 10:43:14 +0200

    WIP - making progress on stencils

--- Source/AmrMesh/CD_EBRedistribution.cpp
+++ Source/AmrMesh/CD_EBRedistribution.cpp
@@ -12,9 +12,12 @@
 // Chombo includes
 #include <CH_Timer.H>
 #include <EBCellFactory.H>
+#include <NeighborIterator.H>
 
 // Our includes
 #include <CD_EBRedistribution.H>
+#include <CD_VofUtils.H>
+#include <CD_BoxLoops.H>
 #include <CD_NamespaceHeader.H>
 
 EBRedistribution::EBRedistribution() noexcept
@@ -78,6 +81,9 @@ EBRedistribution::define(const EBLevelGrid& a_eblgCoar,
     CH_assert(a_refToCoar % 2 == 0);
     CH_assert(a_eblgRefinedCoar.isDefined());
 
+    m_eblgRefinedCoar = a_eblgRefinedCoar;
+    m_eblgCoar        = a_eblgCoar;
+
     m_refToCoar = a_refToCoar;
     m_hasCoar   = true;
   }
@@ -86,6 +92,9 @@ EBRedistribution::define(const EBLevelGrid& a_eblgCoar,
     CH_assert(a_refToFine >= 2);
     CH_assert(a_refToFine % 2 == 0);
     CH_assert(a_eblgCoarsenedFine.isDefined());
+
+    m_eblgCoarsenedFine = a_eblgCoarsenedFine;
+    m_eblgFine          = a_eblgFine;
 
     m_refToFine = a_refToFine;
     m_hasFine   = true;
@@ -110,21 +119,144 @@ EBRedistribution::defineStencils() noexcept
   const DisjointBoxLayout& dbl   = m_eblg.getDBL();
   const EBISLayout&        ebisl = m_eblg.getEBISL();
 
-  if (m_hasCoar) {
-    m_redistStencilsCoar.define(dbl);
+  // These are maps of the valid cells on this level, and cells that lie on the interface. The interfaceCells data is used to figure out
+  // which cells we will redistribute to when we redistribute from a cut-cell and across the coarse-fine interface into a coarse-grid cell. The
+  // validCells is used for 1) restricting which cells we redistribute from, and 2) which fine-grid cells redistribute to.
+  LevelData<BaseFab<bool>> interfaceCellsLD;
+  LevelData<BaseFab<bool>> validCellsLD;
+
+  this->defineValidCells(validCellsLD);
+  this->defineInterfaceCells(interfaceCellsLD);
+
+  m_vofit.define(dbl);
+  m_redistStencilsCoar.define(dbl);
+  m_redistStencilsLevel.define(dbl);
+  m_redistStencilsFine.define(dbl);
+
+  for (DataIterator dit(dbl); dit.ok(); ++dit) {
+    const Box            box            = dbl[dit()];
+    const EBISBox&       ebisbox        = ebisl[dit()];
+    const EBGraph&       ebgraph        = ebisbox.getEBGraph();
+    const IntVectSet     irregIVS       = ebisbox.getIrregIVS(box);
+    const BaseFab<bool>& validCells     = validCellsLD[dit()];
+    const BaseFab<bool>& interfaceCells = interfaceCellsLD[dit()];
+
+    IntVectSet redistCells;
+    for (IVSIterator ivsIt(irregIVS); ivsIt.ok(); ++ivsIt) {
+      const IntVect& iv = ivsIt();
+      if (validCells(iv)) {
+        redistCells |= iv;
+      }
+    }
+
+    VoFIterator&           vofit         = m_vofit[dit()];
+    BaseIVFAB<VoFStencil>& stencilsCoar  = m_redistStencilsCoar[dit()];
+    BaseIVFAB<VoFStencil>& stencilsLevel = m_redistStencilsLevel[dit()];
+    BaseIVFAB<VoFStencil>& stencilsFine  = m_redistStencilsFine[dit()];
+
+    vofit.define(redistCells, ebgraph);
+    stencilsCoar.define(redistCells, ebgraph, 1);
+    stencilsLevel.define(redistCells, ebgraph, 1);
+    stencilsFine.define(redistCells, ebgraph, 1);
+
+    for (vofit.reset(); vofit.ok(); ++vofit) {
+      const VolIndex& vof = vofit();
+
+      // Get the VoFs in the neighborhood of this VoF. When we redistribute, also permit self-redistribution back into this cell.
+      const bool             includeSelf = true;
+      const Vector<VolIndex> neighborVofs =
+        VofUtils::getVofsInRadius(vof, ebisbox, m_redistRadius, VofUtils::Connectivity::SimplyConnected, includeSelf);
+    }
+  }
+}
+
+void
+EBRedistribution::defineValidCells(LevelData<BaseFab<bool>>& a_validCells) const noexcept
+{
+  CH_TIME("EBRedistribution::defineValidCells");
+
+  const DisjointBoxLayout& dbl = m_eblg.getDBL();
+
+  a_validCells.define(dbl, 1, IntVect::Zero);
+  for (DataIterator dit(dbl); dit.ok(); ++dit) {
+    a_validCells[dit()].setVal(true);
   }
 
-  m_redistStencilsLevel.define(dbl);
-
+  // If there's a finer level we need to figure out which cells on the fine level overlap with this level. Then we set those cells
+  // to 'false'. If there's no finer level then all cells on this level are valid cells.
   if (m_hasFine) {
-    m_redistStencilsFine.define(dbl);
+    const DisjointBoxLayout& dblCoFi = m_eblgCoarsenedFine.getDBL();
+
+    // Create some data = 0 on the coarse grid and = 1 on the fine grid.
+    LevelData<FArrayBox> data(dbl, 1, IntVect::Zero);
+    LevelData<FArrayBox> dataCoFi(dblCoFi, 1, IntVect::Zero);
+
+    for (DataIterator dit(dbl); dit.ok(); ++dit) {
+      data[dit()].setVal(0.0);
+    }
+    for (DataIterator dit(dblCoFi); dit.ok(); ++dit) {
+      dataCoFi[dit()].setVal(1.0);
+    }
+
+    dataCoFi.copyTo(data);
+
+    // Go through the coarse grid and set cells to false wherever we find data > 0.0
+    for (DataIterator dit(dbl); dit.ok(); ++dit) {
+      const Box cellBox = dbl[dit()];
+
+      BaseFab<bool>&   validCells = a_validCells[dit()];
+      const FArrayBox& mask       = data[dit()];
+
+      auto kernel = [&](const IntVect& iv) -> void {
+        if (mask(iv) > 0.0) {
+          validCells(iv) = false;
+        }
+      };
+
+      BoxLoops::loop(cellBox, kernel);
+    }
+  }
+}
+
+void
+EBRedistribution::defineInterfaceCells(LevelData<BaseFab<bool>>& a_interfaceCells) const noexcept
+{
+  CH_TIME("EBRedistribution::defineInterfaceCells");
+
+  const DisjointBoxLayout& dbl = m_eblg.getDBL();
+  a_interfaceCells.define(dbl, 1, m_redistRadius * IntVect::Unit);
+
+  for (DataIterator dit(dbl); dit.ok(); ++dit) {
+    a_interfaceCells[dit()].setVal(false);
+  }
+
+  // If there's a coarse grid then some of the cells we would redistribute to overlap with the valid region
+  // on the coarse grid.
+  if (m_hasCoar) {
+    for (DataIterator dit(dbl); dit.ok(); ++dit) {
+      const Box grownBox = grow(dbl[dit()], m_redistRadius);
+
+      DenseIntVectSet divs(grownBox, true);
+
+      NeighborIterator nit(dbl);
+      for (nit.begin(dit()); nit.ok(); ++nit) {
+        divs -= dbl[nit()];
+      }
+      divs -= dbl[dit()];
+
+      // Flag the cells on the other side of the interface.
+      BaseFab<bool>& interfaceCells = a_interfaceCells[dit()];
+      for (DenseIntVectSetIterator ivsIt(divs); ivsIt.ok(); ++ivsIt) {
+        interfaceCells(ivsIt()) = true;
+      }
+    }
   }
 }
 
 void
 EBRedistribution::defineBuffers() noexcept
 {
-  CH_TIMERS("EBRedistribution::defineBuffers");
+  CH_TIME("EBRedistribution::defineBuffers");
 }
 
 void
@@ -139,6 +271,7 @@ EBRedistribution::redistributeAMR(LevelData<EBCellFAB>*             a_phiCoar,
 {
   CH_TIME("EBRedistribution::redistributeAMR");
 
+  CH_assert(m_isDefined);
   CH_assert(a_phi != nullptr);
 
   if (m_hasCoar) {
@@ -164,6 +297,7 @@ EBRedistribution::redistributeCoar(LevelData<EBCellFAB>&             a_phiCoar,
 {
   CH_TIME("EBRedistributino::redistributeCoar");
 
+  CH_assert(m_isDefined);
   CH_assert(a_phiCoar.isDefined());
   CH_assert(a_deltaM.isDefined());
   CH_assert(a_phiCoar.nComp() > a_variables.end());
@@ -177,6 +311,7 @@ EBRedistribution::redistributeLevel(LevelData<EBCellFAB>&             a_phi,
 {
   CH_TIME("EBRedistributino::redistributeLevel");
 
+  CH_assert(m_isDefined);
   CH_assert(a_phi.isDefined());
   CH_assert(a_deltaM.isDefined());
   CH_assert(a_phi.nComp() > a_variables.end());
@@ -191,6 +326,7 @@ EBRedistribution::redistributeFine(LevelData<EBCellFAB>&             a_phiFine,
 {
   CH_TIME("EBRedistributino::redistributeFine");
 
+  CH_assert(m_isDefined);
   CH_assert(a_phiFine.isDefined());
   CH_assert(a_deltaM.isDefined());
   CH_assert(a_phiFine.nComp() > a_variables.end());
